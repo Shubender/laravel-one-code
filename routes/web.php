0ae6de4fd5f5d623bb151407d92c9013d23bd176
shubender <2andrey.verbitskiy@gmail.com>
2023-05-26 19:28:01 +0200

    test routs and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::view('/', 'welcome', [
+    'name' => 'Andy',
+]);
+
+// CRUD (create, read, update, delete)
+
+//Route::get('posts', [PostController::class, 'index'])->name('posts');
+//Route::get('posts/create', [PostController::class, 'create'])->name('posts.create');
+//Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+//Route::get('posts/{post}', [PostController::class, 'show'])->name('posts.show');
+//Route::get('posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+//Route::put('posts/{post}', [PostController::class, 'update'])->name('posts.update');
+//Route::delete('posts/{post}', [PostController::class, 'delete'])->name('posts.delete');
+//Route::put('posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
+
+
+Route::redirect('/test', '/', 302);
+
+Route::get('/controller', TestController::class);
