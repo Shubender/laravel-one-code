9dcd7483910cb06ab2b24bb9df905bc49485446b
shubender <2andrey.verbitskiy@gmail.com>
2023-05-27 14:42:46 +0200

    add ActiveMiddleware.php and check for it

--- routes/user.php
+++ routes/user.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\User\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('user')->middleware('auth')->group(function () {
+Route::prefix('user')->middleware(['auth', 'active'])->group(function () {
     Route::redirect('/', 'user/posts')->name('user');
     Route::get('posts', [PostController::class, 'index'])->name('user.posts');
     Route::get('posts/create', [PostController::class, 'create'])->name('user.posts.create');
