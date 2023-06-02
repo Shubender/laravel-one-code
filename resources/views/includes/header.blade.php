5b8afcd535704ccb381b27316406422e81bd2750
shubender <2andrey.verbitskiy@gmail.com>
2023-06-02 11:00:14 +0200

    add helper file with first func active_link

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -4,35 +4,34 @@
             {{ config('app.name') }}
         </a>
 
-        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar-collapse"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar-collapse" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbar-collapse">
             <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                <li class="nav-item me-3">
-                    <a href="{{ route('home') }}" aria-current="page">
+                <li class="nav-item">
+                    <a href="{{ route('home') }}" class="nav-link {{ active_link('home') }}" aria-current="page">
                         {{ __('Главная') }}
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a href="{{ route('blog') }}" aria-current="page">
+                    <a href="{{ route('blog') }}" class="nav-link {{ active_link('blog*') }}" aria-current="page">
                         {{ __('Блог') }}
                     </a>
                 </li>
             </ul>
 
             <ul class="navbar-nav ms-auto mb-2 mb-md-0">
-                <li class="nav-item me-3">
-                    <a href="{{ route('register') }}" aria-current="page">
+                <li class="nav-item">
+                    <a href="{{ route('register') }}" class="nav-link {{ active_link('register') }}" aria-current="page">
                         {{ __('Регистрация') }}
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a href="{{ route('login') }}" aria-current="page">
+                    <a href="{{ route('login') }}" class="nav-link {{ active_link('login') }}" aria-current="page">
                         {{ __('Вход') }}
                     </a>
                 </li>
