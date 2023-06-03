350c5f6c3ff86574afbfba1c60a11dab14cd52a2
shubender <2andrey.verbitskiy@gmail.com>
2023-06-03 12:11:56 +0200

    create card components for blog posts

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -3,22 +3,24 @@
 @section('page.title', 'Blog Page')
 
 @section('content')
-    <h1 class="mb-5">Blog</h1>
-
-    @if($posts)
-        @foreach($posts as $post)
-            <div class="me-3">
-                <a href="{{ route('blog.show', $post->id) }}">
-                    <h5>
-                        {{$post->title}}
-                    </h5>
-                </a>
-                <p>{{$post->content}}</p>
-            </div>
-        @endforeach
-    @else
-        No Posts yet
-    @endif
+    <section>
+        <x-container>
+            <x-title>
+                {{ __('Posts') }}
+            </x-title>
+            @if($posts)
+                <div class="row">
+                    @foreach($posts as $post)
+                        <div class="col-12 col-md-4">
+                            <x-post.card :post="$post"/>
+                        </div>
+                    @endforeach
+                </div>
+            @else
+                {{ __('No Posts yet') }}
+            @endif
+        </x-container>
+    </section>
 
 @endsection
 
