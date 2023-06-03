812cac20e1cf337e231954f48b4285ddac244b32
shubender <2andrey.verbitskiy@gmail.com>
2023-06-03 12:36:57 +0200

    add back link to title

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,3 +1,11 @@
-<h1 class="mb-5 h2">
-    {{ $slot }}
-</h1>
+<div>
+    @isset($link)
+        <div class="mb-3">
+            {{ $link }}
+        </div>
+    @endisset
+    <h1 class="mb-5 h2">
+        {{ $slot }}
+    </h1>
+</div>
+
