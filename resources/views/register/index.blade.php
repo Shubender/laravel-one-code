b42dfde0cb644ff63107780dfa544a0b695ca5a5
shubender <2andrey.verbitskiy@gmail.com>
2023-06-03 10:36:16 +0200

    create register card

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,8 +1,43 @@
-@extends('layouts.base')
+@extends('layouts.auth')
 
-@section('page.title', 'Registration Page')
+@section('page.title', 'Register Page')
 
-@section('content')
-    <h1>Registration Page</h1>
+@section('auth.content')
+    <x-card>
+        <x-card-header>
+            <x-card-title>
+                {{ __('Регистарция') }}
+            </x-card-title>
+        </x-card-header>
+        <x-card-body>
+            <x-form action="{{ route('register.store') }}" method="POST">
+                @csrf
+                <x-form-item>
+                    <x-label required>{{ __('Name') }}</x-label>
+                    <x-input type="text" name="name" class="form-control" autofocus required/>
+                </x-form-item>
+                <x-form-item>
+                    <x-label required>{{ __('Email') }}</x-label>
+                    <x-input type="email" name="email" class="form-control" required/>
+                </x-form-item>
+                <x-form-item>
+                    <x-label required>{{ __('Password') }}</x-label>
+                    <x-input type="password" name="password" class="form-control" required/>
+                </x-form-item>
+                <x-form-item>
+                    <x-label required>{{ __('Repeat Password') }}</x-label>
+                    <x-input type="password" name="password_confirmation" class="form-control" required/>
+                </x-form-item>
+                <x-form-item>
+                    <x-checkbox name="remember">
+                        {{ __('Я согласен на обработку персональных данных') }}
+                    </x-checkbox>
+                </x-form-item>
+                <x-button type="submit">
+                    {{ __('Зарегистрироваться') }}
+                </x-button>
+            </x-form>
+        </x-card-body>
+    </x-card>
 @endsection
 
