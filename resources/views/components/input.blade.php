a1c74894e52bf9abb4a32bad18c028b25919b0b9
shubender <2andrey.verbitskiy@gmail.com>
2023-06-04 12:59:22 +0200

    fix input component

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,1 +1,8 @@
-<input {{ $attributes }} {{ $slot }} /input>
+@props(['value' => ''])
+
+<input {{ $attributes->class([
+    'form-control',
+])->merge([
+    'type' => 'text',
+    'value' => (old($attributes->get('name')) ?: $value),
+]) }}>
