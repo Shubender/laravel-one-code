4d75405e03afb0a5fe3a5917ca9845a279cfbf84
shubender <2andrey.verbitskiy@gmail.com>
2023-06-04 11:44:41 +0200

    add views and fake posts

--- app/Http/Controllers/User/PostController.php
+++ app/Http/Controllers/User/PostController.php
@@ -8,12 +8,20 @@ class PostController extends Controller
 {
     public function index()
     {
-        return 'Страница список постов';
+        $post = (object) [
+            'id' => 123,
+            'title' => 'Lorem ipsum dolor sit amet.',
+            'content' => 'Lorem ipsum <strong>dolor</strong> sit amet, consectetur adipisicing elit. Iusto, laudantium?'
+        ];
+
+        $posts = array_fill(0, 10, $post);
+
+        return view('user.posts.index', compact('posts'));
     }
 
     public function create()
     {
-        return 'Страница создание поста';
+        return view('user.posts.create');
     }
 
     public function store()
@@ -23,12 +31,12 @@ class PostController extends Controller
 
     public function show($post)
     {
-        return "Страница просмотр поста {$post}";
+        return view('user.posts.show', compact('post'));
     }
 
     public function edit($post)
     {
-        return "Страница изменение поста {$post}";
+        return view('user.posts.edit', compact('post'));
     }
 
     public function update()
