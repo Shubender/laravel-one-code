af38f7e7637a45d95d97de66e5cab09bb2acd15d
shubender <2andrey.verbitskiy@gmail.com>
2023-06-05 09:34:24 +0200

    create trix component

---
+++ resources/views/components/trix.blade.php
@@ -0,0 +1,14 @@
+@props(['name' => ''])
+
+<input id="{{ $name }}" type="hidden" name="{{ $name }}">
+<trix-editor input="{{ $name }}"></trix-editor>
+
+@once
+    @push('css')
+        <link rel="stylesheet" href="/css/trix.css">
+    @endpush
+
+    @push('js')
+        <script src="/js/trix.js"></script>
+    @endpush
+@endonce
