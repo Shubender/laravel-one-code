98f5e2521026ffc38a677c865e96138d1943ec52
shubender <2andrey.verbitskiy@gmail.com>
2023-06-05 09:36:14 +0200

    replace form with component

--- resources/views/user/posts/create.blade.php
+++ resources/views/user/posts/create.blade.php
@@ -13,28 +13,7 @@
         </x-slot>
     </x-title>
 
-    <x-form action="{{ route('user.posts.store') }}" method="POST">
-        <x-form-item>
-            <x-label required>{{ __('Название поста') }}</x-label>
-            <x-input name="title" autofocus/>
-        </x-form-item>
-
-        <x-form-item>
-            <x-label required>{{ __('Содержание поста') }}</x-label>
-            <input id="content" type="hidden" name="content">
-            <trix-editor input="content"></trix-editor>
-        </x-form-item>
-
-        <x-button type="submit">
-            {{ __('Create Post') }}
-        </x-button>
-    </x-form>
+    <x-post.form/>
 @endsection
 
-@push('css')
-    <link rel="stylesheet" href="/css/trix.css">
-@endpush
 
-@push('js')
-    <script src="/js/trix.js"></script>
-@endpush
