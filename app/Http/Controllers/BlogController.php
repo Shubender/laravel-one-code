47eb6cb310ad73822f987403351d928b01431b51
shubender <2andrey.verbitskiy@gmail.com>
2023-06-09 11:45:58 +0200

    add filter for posts

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -7,8 +7,11 @@ use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $search = $request->input('search');
+        $category_id = $request->input('category_id');
+
         $post = (object) [
             'id' => 123,
             'title' => 'Lorem ipsum dolor sit amet.',
@@ -16,6 +19,17 @@ class BlogController extends Controller
         ];
 
         $posts = array_fill(0, 10, $post);
+
+        $posts = array_filter($posts, function ($post) use ($search, $category_id) {
+            if ($search && ! str_contains(strtolower($post->title), strtolower($search))) {
+                return false;
+            }
+
+            if ($category_id && $post->category_id != $category_id) {
+                return false;
+            }
+            return true;
+        });
 
         return view('blog.index', compact('posts'));
     }
